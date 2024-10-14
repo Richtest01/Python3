d07a170676db638daaa70cacddf971287288730f
Richtest01 <137497784+Richtest01@users.noreply.github.com>
2024-10-14 17:49:01 +0100

    Add files via upload

---
+++ List.py
@@ -0,0 +1,14 @@
+mylist =[1,'hello',3.2]
+print(mylist)
+
+# access list elements
+print(mylist[2])
+
+# Checking length of list
+print(len(mylist))
+
+# List contcatation
+list1=[1,2,'Hello', '3.6']
+list2=['This','Is','my','bag']
+print(list1+list2)
+
---
+++ prac.py
@@ -0,0 +1,23 @@
+# mystring = "tinker"
+# print(mystring)
+
+# mystring ="Pai Auniy ji hillo"
+# print(mystring[0])
+# str=mystring[1:]
+# print(str)
+# print("S"+str)
+# letter = 'z'
+# print("2"+str)
+# print(mystring.split('i'))
+# print('This house {r} {b} {e} {n}'.format(n='never',b='busy',r='runs',e='everyday'))
+# result=100999/177
+# print("This result is {r:10.8f}".format(r=result))
+# name="richa screen time for joshi is 10 hrs"
+# # print("my identity is {}".format(name))
+# print(f"my identity is {name}")
+name='richa'
+age=35
+print(f'{name} is {age} years old')
+
+
+
