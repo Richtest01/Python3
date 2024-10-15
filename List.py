72d54bd8502eae3800d6211ca4ca8b50c7eb3801
Richa Sharma <10richtest@gmail.com>
2024-10-15 13:21:24 +0100

    List.py -- new code added test.py -- testing my skills

--- List.py
+++ List.py
@@ -1,14 +1,49 @@
-mylist =[1,'hello',3.2]
-print(mylist)
+mylist = [1,'hello',3.2,8.90,'John']
+# print(mylist)
 
 # access list elements
-print(mylist[2])
+# print(mylist[2])
 
 # Checking length of list
-print(len(mylist))
+# print(len(mylist))
 
 # List contcatation
-list1=[1,2,'Hello', '3.6']
-list2=['This','Is','my','bag']
-print(list1+list2)
+# list1=[1,2,'Hello', '3.6']
+# list2=['This','Is','my','bag']
+# print(list1+list2)
+# list2 = ['india', 'ramesh']
+
+# mylist[1] = 'new name'
+# print(mylist)
+
+# difference between list & string - list items can be update/edit but string cant be changed.Example shown here
+# str = {'hello','ramesh'}
+# print(str)
+# str[1]='updated'
+# print(str)
+
+# Sorting the list --> SOrt doesn't return anything, but if you print the list again, you will found, your list is sorted.
+new_list = ['a','e','f','c','b']
+num_list = [2,5,8,1,3,9]
+new_list.sort()
+print(new_list)
+num_list.sort()
+print(num_list)
+
+# reverse the list--> it will also not return anything like sort, but reverse the list as expected.
+
+new_list.reverse()
+print(new_list)
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ test.py
@@ -0,0 +1,13 @@
+myTuple = (10, 1, 2, 5, 3,4, 7, 8)
+mylist = [10, 1, 2, 5, 3,4, 7, 8]
+
+# print(myTuple)
+# print(mylist)
+
+for x in myTuple:
+    print(x)
+
+    #putput - 10, 1, 2, 5, 3,4, 7, 8
+print("*******************")    
+for y in mylist:
+    print(y)
